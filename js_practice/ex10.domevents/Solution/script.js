249288efb81a93d4033e9caa8eab1f4ded38303b
neoneb <neofelis1nebulosa@gmail.com>
2022-09-03 22:46:38 -0700

    dom manipulation practice

---
+++ js_practice/ex10.domevents/Solution/script.js
@@ -0,0 +1,54 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function createListElement() {
+	var div = document.createElement("div");
+	var li = document.createElement("li");
+	var delButton = document.createElement("button");
+	div.classList.add("wrapper");
+	ul.appendChild(div);
+	div.append(li, delButton);
+	li.classList.add("taskClass");
+	li.appendChild(document.createTextNode(input.value));
+	input.value = "";
+	delButton.classList.add("delClass");
+	delButton.innerHTML='Del';
+}
+
+function inputLength() {
+	return input.value.length;
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function doneTask(task) {
+	if (task.target.tagName === "LI"){
+		task.target.classList.toggle("done");
+	}
+}
+
+function deleteListElement(element) {
+	if (element.target.className === "delClass"){
+		element.target.parentElement.remove();
+	}
+}
+
+function handleUlClick (element) {
+	doneTask(element);
+	deleteListElement(element);
+}
+
+ul.addEventListener("click", handleUlClick)
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
--- landingpage_project/README.txt
+++ landingpage_project/README.txt
@@ -1,3 +1,13 @@
 View completed projects here:
 
 https://neoneb.github.io/zerotomastery_projects/landingpage_project/index.html
+
+
+
+Landing page todo:
+
+1. add footer (this is a practice project)
+2. implement photos page using css grid
+3. have main header image eyes change color on hover
+4. have clickable's pointer change on hover
+5. modify design on join mail box on home page
