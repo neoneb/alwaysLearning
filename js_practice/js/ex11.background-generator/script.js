78c8ba86d7fdc8ea820deb46fabf4f4d477a83f5
neoneb <neofelis1nebulosa@gmail.com>
2022-09-09 20:52:43 -0700

    functions exercise complete / optimization of BG gen script

--- js_practice/js/ex11.background-generator/script.js
+++ js_practice/js/ex11.background-generator/script.js
@@ -8,43 +8,32 @@ color1.value = "#ff0000";
 color2.value = "#ffff00";
 css.textContent = 'linear-gradient(to right, rgb(255, 0, 0), rgb(255, 255, 0));'
 
-function setGradient (e) {
-   body.style.background = 
-   "linear-gradient(to right, " 
-   + color1.value 
-   + ", " 
-   + color2.value 
-   + ")";
-   css.textContent = body.style.background + ";";
+const setGradient = (e) =>{
+   body.style.background = `linear-gradient(to right,${color1.value}, ${color2.value})`; 
+   css.textContent = `${body.style.background};`;
 }
 
-function randomize () {
-   let red1 = Math.floor(Math.random() * 256);
-   let green1 = Math.floor(Math.random() * 256);
-   let blue1 = Math.floor(Math.random() * 256);
-   let red2 = Math.floor(Math.random() * 256);
-   let green2 = Math.floor(Math.random() * 256);
-   let blue2 = Math.floor(Math.random() * 256);
-   ////DRY--- add separate function for math randomized
-   const random1 = 'rgb(' + red1 + ', ' + green1 + ', ' + blue1 + ')';
-   const random2 = 'rgb(' + red2 + ', ' + green2 + ', ' + blue2 + ')';
-   body.style.background = 'linear-gradient(to right, ' 
-   + random1
-   + ', ' 
-   + random2 
-   + ')'; 
-   css.textContent = body.style.background + ";";
-   // + stuff can be done with ES6 `${}`
+const randomize = () => {
+   const colorArray = [0, 1 ,2 ,3 ,4 ,5];
+   const mapColorArray = colorArray.map((color) => {
+         return Math.floor(Math.random() * 256); 
+      });
 
-function ColorToHex(color) {
-   var hexadecimal = color.toString(16);
+   const random1 = `rgb(${mapColorArray[0]}, ${mapColorArray[1]}, ${mapColorArray[2]})`;
+   const random2 = `rgb(${mapColorArray[3]}, ${mapColorArray[4]}, ${mapColorArray[5]})`;
+
+   body.style.background = `linear-gradient(to right,${random1}, ${random2})`; 
+   css.textContent = `${body.style.background};`;
+
+   const ColorToHex = (color) => {
+   const hexadecimal = color.toString(16);
    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
  }
- function ConvertRGBtoHex(red, green, blue) {
-   return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+   const ConvertRGBtoHex = (red, green, blue) => {
+   return `#${ColorToHex(red)}${ColorToHex(green)}${ColorToHex(blue)}`;
  }
-   let randomHex1 = ConvertRGBtoHex(red1, green1, blue1);
-   let randomHex2 = ConvertRGBtoHex(red2, green2, blue2);
+   const randomHex1 = ConvertRGBtoHex(mapColorArray[0], mapColorArray[1], mapColorArray[2]);
+   const randomHex2 = ConvertRGBtoHex(mapColorArray[3], mapColorArray[4], mapColorArray[5]);
 
    color1.value = randomHex1;
    color2.value = randomHex2;
