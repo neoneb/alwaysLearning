1281764811dffda2834f89ea44bfc5d2caf490d9
neoneb <neofelis1nebulosa@gmail.com>
2022-09-17 15:04:09 -0700

    added photos section, added hover effect for header img

---
+++ landingpage_project/js/script.js
@@ -0,0 +1,27 @@
+
+const toggler = document.querySelector ('.navbar-toggler');
+const altHead = document.querySelector ('#head-img-alt');
+
+const imageMover = () =>  {
+   altHead.classList.toggle('move-img');
+  
+}
+
+const toggleOnTouch = () => {
+   altHead.classList.toggle('touch-toggle');
+}
+
+
+
+
+
+document.addEventListener('touchstart', toggleOnTouch);
+document.addEventListener('touchend', toggleOnTouch);
+
+toggler.addEventListener ('click', imageMover);
+
+
+
+
+
+
