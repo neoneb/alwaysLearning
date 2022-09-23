7b6f44d67350149bfece8dc72ab9447addf0881a
neoneb <neofelis1nebulosa@gmail.com>
2022-09-23 10:07:06 -0700

    npm & node introduction

--- constant-learning/zerotomastery_coursework/js_practice/js/ex11.background-generator/script.js
+++ constant-learning/zerotomastery_coursework/js_practice/js/ex11.background-generator/script.js
@@ -1,3 +1,9 @@
+const _ = require('lodash');
+
+const array = [1,2,3,4,5,6,7,8];
+console.log('answer', _.without(array, 3));
+
+
 const css = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
