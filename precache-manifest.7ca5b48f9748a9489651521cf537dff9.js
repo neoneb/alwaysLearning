720a9d4856b2c351ec2c55251e95ea0b964dffc7
neoneb <neofelis1nebulosa@gmail.com>
2022-09-26 10:34:49 -0700

    Updates

---
+++ precache-manifest.7ca5b48f9748a9489651521cf537dff9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4bd875f6edd1eb0e21",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/css/main.e73d7298.chunk.css"
+  },
+  {
+    "revision": "2f4bd875f6edd1eb0e21",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/js/main.a795f5fd.chunk.js"
+  },
+  {
+    "revision": "1163ea9de381a9ac193b",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/js/runtime~main.6f45373f.js"
+  },
+  {
+    "revision": "61e5a9480cddb6b18312",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/css/2.fc4519ed.chunk.css"
+  },
+  {
+    "revision": "61e5a9480cddb6b18312",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/js/2.3cba9a74.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "996159687aac53fbb2a2af081fb58273",
+    "url": "/neoneb-main/constant-learning/zerotomastery_coursework/react_practice/create-react-app/my-app/index.html"
+  }
+];
