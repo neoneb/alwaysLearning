9a0e9315a1726454b0ea58df5c7b6a98124d3736
neoneb <neofelis1nebulosa@gmail.com>
2022-10-01 16:20:45 -0700

    Started new JS coS course

---
+++ constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,49 @@
+//LECTURE: Values and Variables
+//1.
+
+const country = 'Canada';
+const continent = 'North America';
+const population = '38 Million';
+
+//2.
+
+console.log(country, continent, population);
+
+// LECTURE: Data Types
+//1.
+
+const isIsland = false;
+var language;
+
+//2.
+
+console.log(isIsland, population, country, language)
+
+//LECTURE: let, const and var
+//1.
+
+const language = 'English;'
+
+//2.
+
+const country = 'Canada';
+const continent = 'North America';
+let population = '38 Million';
+const isIsland = false;
+
+//3.
+
+continent = 'South America';
+console.log(continent)
+
+//LECTURE: Basic Operators
+
+//1.
+
+//2.
+
+//3.
+
+//4.
+
+//5.
---
+++ constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+//    let js = 'amazing';
+//    if (js === 'amazing') alert('JavsScript is Fun');
+
+//    40 + 8 + 23 - 10;
+//    console.log(40 + 8 + 23 - 10)
+
+//    console.log('Jonas');
+//    console.log(23);
+
+//    let firstName = 'Jonas';
+//    console.log(firstName);
+//    console.log(firstName);
+//    console.log(firstName);
+
+// let javascriptIsFun = true;
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Chris');
+
+let age = 30;
+age = 31;
+
+console.log(age)
