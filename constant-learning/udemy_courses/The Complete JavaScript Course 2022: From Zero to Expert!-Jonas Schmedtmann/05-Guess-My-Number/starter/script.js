47a8b5b9d8508353672d0e8d26aae9ff43fdeec0
neoneb <neofelis1nebulosa@gmail.com>
2022-10-14 21:11:11 -0700

    finished up to part 78

--- constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/05-Guess-My-Number/starter/script.js
+++ constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,50 @@
 'use strict';
+let secretNumber = Number(1 + Math.floor((Math.random() * 19).toFixed(0)));
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
-console.log(document.querySelector(`.message`).textContent);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
 
-document.querySelector(`.message`).textContent = 'Correct Number!';
+  // When there is no input
+  if (!guess) {
+    displayMessage('🚨No Number Detected');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🥳Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '⬇️ Try Lower' : '⬆️ Try Higher');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 Game Over');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.score').textContent = 20;
+  secretNumber = Number(1 + Math.floor((Math.random() * 19).toFixed(0)));
+  displayMessage('Start guessing...');
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+});
