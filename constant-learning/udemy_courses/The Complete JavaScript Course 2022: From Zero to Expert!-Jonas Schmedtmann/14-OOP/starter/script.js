b64fb79209b19784daf283896631c0cb0f16affc
neoneb <87101137+neoneb@users.noreply.github.com>
2023-02-10 16:41:12 -0800

    finished to sec 210

--- constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/14-OOP/starter/script.js
+++ constant-learning/udemy_courses/The Complete JavaScript Course 2022: From Zero to Expert!-Jonas Schmedtmann/14-OOP/starter/script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+// Constructor Functions
+////////////////////////////////////
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', '1991');
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function called, this = {} (object)
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+console.log(jay instanceof Person);
+
+// Prototypes
+//////////////////////////////////
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototypeOfLinkedObjects <-- should be but isn't
+
+Person.prototype.species = 'Home Sapiens';
+console.log(jonas.species, matilda);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
