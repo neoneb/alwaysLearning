929352a86a87921077255c08815b5950ab681f2d
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-05 14:45:43 -0800

    finished 75% of ZTM course

--- constant-learning/zerotomastery_coursework/js_practice/advanced-js/smartbrainproject/my-app/src/App.js
+++ constant-learning/zerotomastery_coursework/js_practice/advanced-js/smartbrainproject/my-app/src/App.js
@@ -1,25 +1,61 @@
 import './App.css';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <>
-        <div>...</div>
-        <ParticlesBg type='lines' bg={true} />
-      </>
+const app = new Clarifai.App({ apiKey: '2f4a7aadfb484d0fb1c3bb1c31dfd0d5' });
 
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState = { imageUrl: this.state.input };
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function (response) {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      function (err) {
+        // There was an error
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <>
+          <div>...</div>
+          <ParticlesBg type='lines' bg={true} />
+        </>
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- constant-learning/zerotomastery_coursework/js_practice/advanced-js/smartbrainproject/my-app/src/components/ImageLinkForm/ImageLinkForm.js
+++ constant-learning/zerotomastery_coursework/js_practice/advanced-js/smartbrainproject/my-app/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
@@ -9,8 +9,15 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='btn w-30 grow f4 link ph3 pv2 dib white bg-green'>
+          <input
+            className='f4 pa2 w-70 center'
+            type='text'
+            onChange={onInputChange}
+          />
+          <button
+            className='btn w-30 grow f4 link ph3 pv2 dib white bg-green'
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
