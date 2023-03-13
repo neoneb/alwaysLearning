eafb0b838397f84fac0a8180a03ffaa81f4a0a6e
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-13 10:11:42 -0700

    finished 81% ZTM web dev course

--- constant-learning/zerotomastery_coursework/js_practice/nodejs_practice/expressjs/server.js
+++ constant-learning/zerotomastery_coursework/js_practice/nodejs_practice/expressjs/server.js
@@ -1,26 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('<h1>Hello!</h1>');
-  next();
-});
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.send('getting root');
-});
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use((req, res, next) => {
+//   console.log('<h1>Hello!</h1>');
+//   next();
+// });
 
-app.get('/profile', (req, res) => {
-  res.send('getting profile');
-});
+// app.get('/', (req, res) => {
+// console.log(req.query);
+// req.body;
+// console.log(req.headers);
+// req.params;
+// res.status(404).res.send('not found');
+// });
 
-app.post('/profile', (req, res) => {
-  const user = {
-    name: 'sally',
-    hobby: 'soccer',
-  };
-  res.send(user);
-});
+// app.get('/profile', (req, res) => {
+//   res.send('getting profile');
+// });
+
+// app.post('/profile', (req, res) => {
+//   console.log(req.body);
+//   res.send('Success');
+// });
 
 app.listen(3000);
