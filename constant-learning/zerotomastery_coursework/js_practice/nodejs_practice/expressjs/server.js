b767570839b21931b82a3a501dfc028f94d924b6
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-14 11:18:08 -0700

    finished ZTM web dev course up to 82%

--- constant-learning/zerotomastery_coursework/js_practice/nodejs_practice/expressjs/server.js
+++ constant-learning/zerotomastery_coursework/js_practice/nodejs_practice/expressjs/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const app = express();
+// const app = express();
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
@@ -29,4 +29,42 @@ app.use(express.static(__dirname + '/public'));
 //   res.send('Success');
 // });
 
-app.listen(3000);
+// app.listen(3000);
+
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+const fs = require('fs');
+
+fs.readFile('./hello.txt', (err, data) => {
+  console.time('funchallenge');
+  if (err) {
+    console.log('error!');
+  }
+  console.log('Async:', data.toString('utf8'));
+  console.timeEnd('funchallenge');
+});
+
+const file = fs.readFileSync('./hello.txt');
+console.log('Sync:', file.toString('utf8'));
+
+// APPEND
+// fs.appendFile('./hello.txt', ' This is so cool!', (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+//WRITE
+// fs.writeFile('bye.txt', 'sad to see you go', (err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// });
+
+// DELETE
+fs.unlink('./bye.txt', (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Inception');
+});
