48c7d262f5df61e2baa8739e1020ccd35f286b28
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-15 11:14:22 -0700

    finished 83% ZTM web dev course

---
+++ constant-learning/zerotomastery_coursework/js_practice/advanced-js/smartbrainproject/smartBrainApi/server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'john',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+// Signin --> POST success/fail
+app.post('/signin', (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
+  } else {
+    res.status(400).json('error logging in');
+  }
+});
+
+// register --> POST user
+app.post('/register', (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: '125',
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+// profile/:userId --> GET = user
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
+
+// image --> PUT --> user
+
+app.listen(3000, () => {
+  console.log('app is running on port 3000');
+});
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json('not found');
+  }
+});
