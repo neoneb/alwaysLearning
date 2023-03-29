04cd4dbc76a38922bfa25db2162b8d09260ddee5
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-29 11:15:30 -0700

    started code wars challenge #7

---
+++ constantLearning/codeChalleges/codeWars/7.js
@@ -0,0 +1,25 @@
+// DESCRIPTION:
+// This kata is all about adding numbers.
+
+// You will create a function named add. It will return the sum of all the arguments. Sounds easy, doesn't it?
+
+// Well Here's the Twist. The inputs will gradually decrease with their index as parameter to the function.
+
+//   add(3,4,6);
+//   /*
+//   returns ( 3 / 1 ) + ( 4 / 2 ) + ( 6 / 3 ) = 7
+//   */
+// Remember the function will return 0 if no arguments are passed and it must round the result if sum is a float.
+
+// Example
+
+//   add(); //=> 0
+//   add(1,2,3); //=> 3
+//   add(1,4,-6,20); //=> 6
+// Check my another kata here!!
+
+// https://www.codewars.com/kata/555de49a04b7d1c13c00000e/train/javascript
+
+function add() {
+  return 0;
+}
