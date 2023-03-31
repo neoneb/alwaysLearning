1d8c512643b41672753ffc06b34bc1a6c0d11828
neoneb <87101137+neoneb@users.noreply.github.com>
2023-03-30 20:16:27 -0700

    start of slide up projects page / button functionality / styling tweaks

--- projects/portfolioWebsite/dist/index.f5c48570.js
+++ projects/portfolioWebsite/dist/index.f5c48570.js
@@ -557,6 +557,15 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"8ZNvh":[function(require,module,exports) {
+const projectsBtn = document.querySelector(".projects__btn");
+const projectsContainer = document.querySelector(".projects__Container");
+const backBtn = document.querySelector(".back__button");
+projectsBtn.addEventListener("click", function() {
+    projectsContainer.classList.add("show");
+});
+backBtn.addEventListener("click", function() {
+    projectsContainer.classList.remove("show");
+});
 
 },{}]},["47Zjz","8ZNvh"], "8ZNvh", "parcelRequiree1fe")
 
--- projects/portfolioWebsite/readme.md
+++ projects/portfolioWebsite/readme.md
@@ -3,3 +3,4 @@ Chris Portfolio Website
 Versions
 1.0.0 - Initial markup
 1.0.1 - Initial styling
+1.0.2 - start of slide up projects page / button functionality / styling tweaks
