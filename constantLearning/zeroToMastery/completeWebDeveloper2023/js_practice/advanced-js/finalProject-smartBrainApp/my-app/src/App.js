0cff169b9ed972fd5518da4395f72cded5fa162c
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-02 11:12:38 -0700

    change server location

--- constantLearning/zeroToMastery/completeWebDeveloper2023/js_practice/advanced-js/finalProject-smartBrainApp/my-app/src/App.js
+++ constantLearning/zeroToMastery/completeWebDeveloper2023/js_practice/advanced-js/finalProject-smartBrainApp/my-app/src/App.js
@@ -69,7 +69,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://smart--brain-app.herokuapp.com/imageurl', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -82,7 +82,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smart--brain-app.herokuapp.com/image', {
             method: 'put',
             headers: {
               'Content-Type': 'application/json',
--- constantLearning/zeroToMastery/completeWebDeveloper2023/js_practice/advanced-js/finalProject-smartBrainApp/my-app/src/components/SignIn/SignIn.js
+++ constantLearning/zeroToMastery/completeWebDeveloper2023/js_practice/advanced-js/finalProject-smartBrainApp/my-app/src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://smart--brain-app.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
