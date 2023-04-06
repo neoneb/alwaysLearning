af73f603a8da763ddd07f1dbfae24d02ab69d22d
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-05 18:53:18 -0700

    finished code wars challenge #8

---
+++ constantLearning/codeChalleges/codeWars/8.js
@@ -0,0 +1,8 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+  return bool === true ? 'Yes' : 'No';
+}
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
