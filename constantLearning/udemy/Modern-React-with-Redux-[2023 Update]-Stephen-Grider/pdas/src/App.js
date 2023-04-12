79c5aec8dde7eca4410f6fdbbef94b9f6f44187a
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-12 16:33:56 -0700

    finished up to part 40 of React course

--- constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/pdas/src/App.js
+++ constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/pdas/src/App.js
@@ -1,12 +1,48 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './imgs/alexa.png';
+import CortanaImage from './imgs/cortana.png';
+import SiriImage from './imgs/siri.png';
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+          <p className='title'>Personal Digital Assistants</p>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className='section'>
+          <div className='columns'>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Alexa'
+                handle='@alexa99'
+                image={AlexaImage}
+                description='Alexa was created by Amazon and helps you buy things.'
+              />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Cortana'
+                handle='@cortana32'
+                image={CortanaImage}
+                description='Cortana was made by Microsoft. Who knows what it does?'
+              />
+            </div>
+            <div className='column is-4'>
+              <ProfileCard
+                title='Siri'
+                handle='@siri01'
+                image={SiriImage}
+                description='Siri was made by Apple and is being phased out.'
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/pdas/src/ProfileCard.js
+++ constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/pdas/src/ProfileCard.js
@@ -1,5 +1,22 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard({ title, handle, image, description }) {
+  const altText = `${title} Logo`;
+  return (
+    <div className='card'>
+      <div className='card-image'>
+        <figure className='image is-1by1'>
+          <img src={image} alt={altText} />
+        </figure>
+      </div>
+
+      <div className='card-content'>
+        <div className='media-content'>
+          <p className='title is-4'>{title}</p>
+          <p className='subtitle is-6'>{handle}</p>
+        </div>
+        <div className='content'>{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
