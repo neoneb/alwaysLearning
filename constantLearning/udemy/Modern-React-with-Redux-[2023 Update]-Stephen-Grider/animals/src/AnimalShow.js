112944f5b72a69e2a475a676aa8b9925479b89ae
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-13 20:26:13 -0700

    finished up to part 62 of 433 react course

---
+++ constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/animals/src/AnimalShow.js
@@ -0,0 +1,5 @@
+function AnimalShow() {
+  return <div>Cow!</div>;
+}
+
+export default AnimalShow;
---
+++ constantLearning/udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/animals/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
+    </div>
+  );
+}
+
+export default App;
