04e9140ed60ebad8ce45d6092ff0e3999bc548a7
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-18 11:31:38 -0700

    finsished code wars challenge #15

---
+++ codeChalleges/codeWars/15.js
@@ -0,0 +1,9 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+function getSum(a, b) {
+  return a !== b ? a + b : a;
+}
+
+console.log(getSum(155, -11));
