f617cc07c2967aaa91408d416003f8ea9c3b1fb2
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-30 13:41:32 -0700

    finished up to part 315 of 433 React course

--- udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/comps/src/pages/CounterPage.js
+++ udemy/Modern-React-with-Redux-[2023 Update]-Stephen-Grider/comps/src/pages/CounterPage.js
@@ -1,54 +1,66 @@
+import { produce } from 'immer';
 import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
-const reducer = (state, action) => {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      count: state.count + 1,
-    };
-  }
+const INCREMENT_COUNT = 'increment';
+const SET_VALUE_TO_ADD = 'change-value-to-add';
+const DECREMENT_COUNT = 'decrement';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
-  if (action.type === 'change-value-to-add') {
-    return {
-      ...state,
-      valueToAdd: action.payload,
-    };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1;
+      return;
+    case DECREMENT_COUNT:
+      state.count = state.count - 1;
+      return;
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload;
+      return;
+    case ADD_VALUE_TO_COUNT:
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
+    default:
+      return;
   }
-  return state;
 };
 
 function CounterPage({ initialCount }) {
   //   const [count, setCount] = useState(initialCount);
   //   const [valueToAdd, setValueToAdd] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, dispatch] = useReducer(produce(reducer), {
     count: initialCount,
     valueToAdd: 0,
   });
 
   const increment = () => {
     dispatch({
-      type: 'increment',
+      type: INCREMENT_COUNT,
     });
   };
 
   const decrement = () => {
-    //  setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
 
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0;
     dispatch({
-      type: 'change-value-to-add',
+      type: SET_VALUE_TO_ADD,
       payload: value,
     });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //  setCount(count + valueToAdd);
-    //  setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
 
   return (
