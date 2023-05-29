a8a6937b3c12e09684c2d05134ea61ecc651c95d
neoneb <87101137+neoneb@users.noreply.github.com>
2023-05-29 10:21:54 -0700

    finished code wars challenge #23

---
+++ codeChalleges/codeWars/23.js
@@ -0,0 +1,50 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+const alphabet = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+function alphabetPosition(text) {
+  const array = text.toLowerCase().split('');
+  return array
+    .filter((letter) => alphabet.includes(letter))
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
