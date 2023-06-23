399aa61cde6c8b6508918fab32ba8d33d5549d92
neoneb <87101137+neoneb@users.noreply.github.com>
2023-06-23 11:45:54 -0700

    finished codewars challenge #29

--- codeChalleges/codeWars/29.js
+++ codeChalleges/codeWars/29.js
@@ -6,6 +6,11 @@
 // Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 // Jaden-Cased: "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
-String.prototype.toJadenCase = function () {
-  //...
+const toJadenCase = function (str) {
+  return str
+    .split(' ')
+    .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
+    .join(' ');
 };
+
+console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
